a34e20d1cd127974b0c55f6a727bd2b319ce611a
Matt Shinall <matt.shinall@gmail.com>
2019-07-24 17:03:02 -0400

    added weather to clock screen

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import os
 import re
 from numpy import interp
 import geocoder
+import requests
+from bs4 import BeautifulSoup
 
 ink = InkyPHAT("black")
 ink.set_border(ink.WHITE)
@@ -28,13 +30,29 @@ font_small = ImageFont.truetype("/usr/local/lib/python2.7/dist-packages/font_sou
 btn.set_pixel(0, 0, 0)
 
 is_busy = False
+wth_summary_map = {
+	"snow":                "Snow",
+	"sleet":               "Sleet",
+	"rain":                "Rain",
+	"fog":                 "Fog",
+	"cloudy":              "Cloudy",
+	"partly-cloudy-day":   "Partly Cloudy",
+	"partly-cloudy-night": "Partly Cloudy",
+	"clear-day":           "Clear",
+	"clear-night":         "Clear",
+	"wind":                "Wind"
+}
 
 def blink():
 	btn.set_pixel(0,255,0)
         time.sleep(0.1)
         btn.set_pixel(0,0,0)
 
-def text(x, y, text, font):
+def text(x, y, text, font):		
+	if(x < 0):
+		[w, h] = font.getsize(text);
+		x = (ink.WIDTH / 2) - (w / 2)	
+
 	draw.text((x, y), text, ink.BLACK, font=font)
 	#print("[" + str(x) + "," +str(y) + "] " + text);
 
@@ -47,6 +65,22 @@ def winfo():
 	s = int(interp(n, [0, d], [0, 100]))
 	#print(id + " " + str(n) + "/" + str(d) + " " + str(s) + "/" + str(width))
 	return [id, s]
+
+def weather(coords):
+    weather = {}
+    res = requests.get("https://darksky.net/forecast/{}/us12/en".format(",".join([str(c) for c in coords])))
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content, "lxml")
+        curr = soup.find_all("span", "currently")
+	#print(curr)
+        weather["summary"] = curr[0].img["alt"].split()[0]
+        weather["temperature"] = int(curr[0].find("span", "summary").text.split()[0][:-1])
+        press = soup.find_all("div", "pressure")
+        weather["pressure"] = int(press[0].find("span", "num").text)
+	weather["short_summary"] = wth_summary_map[weather["summary"]]
+        return weather
+    else:
+        return weather
 
 def busy():
 	global is_busy
@@ -65,9 +99,9 @@ def free():
 		return True
 def do(func):
 	if(busy()):
-		print("busy: " + func.__name__)
+		#print("busy: " + func.__name__)
 		return
-	print("free: " + func.__name__)
+	#print("free: " + func.__name__)
 	blink()
 	btn.set_pixel(0, 0, 127)
 	func()
@@ -88,21 +122,15 @@ def blank():
 def clock():
 	clear()
 	now = localtime()
-	text(30, 0, strftime("%a %b %d %Y", now), font_large)
-	text(45, 22, strftime("%I:%M:%S %p", now), font_large)
+	text(-1, 0, strftime("%a %b %d %Y", now), font_large)
+	text(-1, 22, strftime("%I:%M:%S %p", now), font_large)
 
 	here = geocoder.ip('me')
-	text(30, 22*2, here.city + " " + here.state, font_large)
-
-	show()
-
-def ip():
-	clear()
-	[wfid, wfsig] = winfo()
-	myip = os.popen('echo `ifconfig | grep -A1 wlan0` | awk \'{print $6}\'').read()
-	text(10, 0,  "ssid: " + wfid, font_large)
-	text(10, 30, "str: " + str(wfsig) + "%", font_large)
-	text(10, 60, "ip: " +  myip, font_large)
+	text(-1, 22*2, here.city + " " + here.state, font_large)
+	
+	wth = weather(here.latlng)
+	#print(wth)
+	text(-1, 22*3, str(wth["temperature"]) + "*F " + wth["short_summary"], font_large)
 	show()
 
 def info():
@@ -111,30 +139,30 @@ def info():
 	tm = re.search("temp=([\d]*).?([\d]*)'C", temp)
 	tc = int(tm.group(1))
 	tf = tc * (9 / 5) + 32
-	text(10, 0, "temp: " + str(tf) + "'F / " + str(tc) + "'C", font_small)
+	text(0, 0, "temp: " + str(tf) + "'F / " + str(tc) + "'C", font_small)
 
 	volt = os.popen("vcgencmd measure_volts").read()
 	vm = re.search("volt=([\d]*).?([\d]*)V", volt)
 	v = float(vm.group(1) + "." + vm.group(2))
-	text(10, 16, "volts: " + str(v) + "v", font_small)
+	text(0, 16, "volts: " + str(v) + "v", font_small)
 
 	stor = os.popen("echo `df -h | grep /dev/root | awk '{ print $4\"/\"$2 }'`").read()
-	text(10, 16*2, "disk: " + str(stor), font_small)
+	text(0, 16*2, "disk: " + str(stor), font_small)
 
 	up = os.popen("uptime").read()
-	um1 = re.search("[\d:]*\s+up\s+([\d:]*)", up)
-	um2 = re.search("[\d:]*\s+up\s+([\d]*)\s+(\w*),", up)
+	um1 = re.search("[\d:]*\s+up\s+(\d+):(\d+)", up)
+	um2 = re.search("[\d:]*\s+up\s+(\d*)\s+(\w*),", up)
 	if(um2 and um2.group(1) and um2.group(2)):
-		text(10, 16*3, "up: " + str(um2.group(1) + " " + um2.group(2)), font_small)
-	elif(um1 and um1.group(1)):
-		text(10, 16*3, "up: " + str(um1.group(1)), font_small)
+		text(0, 16*3, "up: " + str(um2.group(1) + " " + um2.group(2)), font_small)
+	elif(um1 and um1.group(1) and um1.group(2)):
+		text(0, 16*3, "up: " + str(um1.group(1)) + " hours and " + str(um1.group(2)) + " mins", font_small)
 	else:
-		text(10, 16*3, "up: (error)", font_small)
+		text(0, 16*3, "up: (error)", font_small)
 
 	[wfid, wfsig] = winfo()
 	myip = os.popen('echo `ifconfig | grep -A1 wlan0` | awk \'{print $6}\'').read()
-	text(10, 16*4,  "wifi: " + wfid + " (" + str(wfsig) + "%)", font_small)
-	text(10, 16*5, "ip: " +  myip, font_small)
+	text(0, 16*4,  "wifi: " + wfid + " (" + str(wfsig) + "%)", font_small)
+	text(0, 16*5, "ip: " +  myip, font_small)
 	show()
 
 @btn.on_hold(btn.BUTTON_E, hold_time=1)
@@ -142,10 +170,6 @@ def handler(button):
 	do(clock)
 
 @btn.on_hold(btn.BUTTON_D, hold_time=1)
-def handler(button):
-	do(ip)
-
-@btn.on_hold(btn.BUTTON_B, hold_time=1)
 def handler(button):
 	do(info)
 
