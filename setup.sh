d425a1eb0aca336f839ccbdd2f9a7c83d3cfddc7
Matt Shinall <matt.shinall@gmail.com>
2022-09-05 12:47:41 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing package dependencies with pip..."
-for module in inky inkyphat Pillow buttonshim numpy geocoder requests bs4; do
+for module in inky inkyphat pillow buttonshim numpy geocoder requests bs4; do
 	echo "Installing $module..."
 	pip install $module;
 	done
